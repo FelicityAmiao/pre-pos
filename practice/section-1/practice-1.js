edf983beecbd145edc8837ae93e3ab81979c3ca5
FelicityAmiao <1760077814@qq.com>
2019-07-09 02:04:18 +0800

    选出A集合中与B集合中相同的元素

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var result = [];
+  collectionA.forEach(function(item, index) {
+    collectionB.forEach(function(itemB, indexB) {
+      if(item === itemB) {
+        result.push(item);
+      }
+    });
+  });
+  return result;
 }
