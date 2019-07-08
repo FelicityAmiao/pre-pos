73393782129acab835eed6875bf25514ff730803
FelicityAmiao <1760077814@qq.com>
2019-07-09 02:07:17 +0800

    选出A集合中元素的key属性跟B对象中value属性中的元素相同的元素,把他们的count，满3减1，输出减过之后的新A集

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var bArr = objectB['value'];
+  collectionA.forEach(function(obj) {
+    bArr.forEach(function(item) {
+      if(obj['key'] === item) {
+        obj['count'] < 3? obj['count']--: obj['count']-= (Math.floor(obj['count'] / 3));
+      }
+    });
+  });
+  return collectionA;
 }
