ef6196559634bf234215d30686d9f75c08e5813c
FelicityAmiao <1760077814@qq.com>
2019-07-09 02:06:31 +0800

    把A集合中相同的元素统计出数量

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var countObjs = {};
+  collection.reduce(function(obj, currentValue) {
+    if(currentValue.length === 1) {
+      obj[currentValue] ? obj[currentValue]++ : obj[currentValue] = 1;
+    }else {
+      let splitArr = currentValue.split("-");
+      obj[splitArr[0]] ? obj[splitArr[0]]+= parseInt(splitArr[1]): obj[splitArr[0]] = parseInt(splitArr[1]);
+    }
+    return obj;
+  }, countObjs);
+  var result = [];
+  for(let key in countObjs) {
+    let obj = {};
+    obj['key'] = key;
+    obj['count'] = countObjs[key];
+    result.push(obj);
+  }
+  return result;
 }
