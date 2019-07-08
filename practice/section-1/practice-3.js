f2d6050e102b8f15a4ffebc00bec91783d9c147d
FelicityAmiao <1760077814@qq.com>
2019-07-09 02:05:15 +0800

    选出A集合中跟B对象中value属性中的元素相同的元素

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var result = [];
+  var cc = objectB['value'];
+  collectionA.forEach(function(item, index) {
+    cc.forEach(function(itemB, indexB) {
+      if(item === itemB) {
+        result.push(item);
+      }
+    });
+  });
+  return result;
 }
