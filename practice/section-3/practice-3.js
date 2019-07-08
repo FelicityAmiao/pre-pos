d2b6d5c6ab66a461878ce1e437512f5f62e8ec61
FelicityAmiao <1760077814@qq.com>
2019-07-09 02:07:30 +0800

    统计出A集合中相同的元素的个数，形成C集合，C集合中的元素要形如{key:"a", count: 3}，然后选出C集合中的元素的key属性跟B对象中value属性中的元素相同的元素,把他们的count，满3减1，输出减过之后的新C集合

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,25 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var cObj = {};
+  collectionA.reduce(function(obj, curr) {
+    obj[curr]? obj[curr]++: obj[curr] = 1;
+    return obj;
+  }, cObj);
+  var bArr = objectB['value'];
+  for(let key in cObj) {
+    bArr.forEach(function(item) {
+      if(key === item) {
+        cObj[key] < 3? cObj[key]--: cObj[key] -= Math.floor(cObj[key] / 3);
+      }
+    });
+  }
+  var resultArr = [];
+  for(let key in cObj) {
+    let rObj = {};
+    rObj['key'] = key;
+    rObj['count'] = cObj[key];
+    resultArr.push(rObj);
+  }
+  return resultArr;
 }
